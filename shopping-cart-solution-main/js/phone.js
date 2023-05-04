8d607ada14d0f6aef1a6173afc9ce9cc03c58aa8
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-04 23:57:30 +0600

    phone js file added

---
+++ shopping-cart-solution-main/js/phone.js
@@ -0,0 +1,18 @@
+function updatePhoneNumber (isIncrease){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    let newPhoneNumber;
+
+    if(isIncrease){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else{
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneNumberField.value = newPhoneNumber;
+    
+    return newPhoneNumber;
+}
+
